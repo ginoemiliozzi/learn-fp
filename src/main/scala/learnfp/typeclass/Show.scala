3c6bf418dbbbb0254308f55b1356b641bbcb015b
Gino Emiliozzi <ginoemiliozzi11@gmail.com>
2019-11-21 14:10:34 -0300

    totalOrder and Show impl

--- src/main/scala/learnfp/typeclass/Show.scala
+++ src/main/scala/learnfp/typeclass/Show.scala
@@ -12,14 +12,23 @@ object Printer {
 
 object ShowInstances {
   implicit val intInstance:Show[Int] = new Show[Int] {
-    override def show(x: Int): String = ???
+    override def show(x: Int): String = x.toString
   }
 
   implicit val doubleInstance:Show[Double] = new Show[Double] {
-    override def show(x: Double): String = ???
+    override def show(x: Double): String = x.toString
   }
 
   implicit def listInstance[T](implicit xShow:Show[T]):Show[List[T]] = new Show[List[T]] {
-    override def show(xs:List[T]): String = ???
+    override def show(xs:List[T]): String = {
+      val lastIndex = xs.size - 1
+      xs.zipWithIndex.foldLeft("["){case (res, (e, idx)) =>
+        if(idx != lastIndex) {
+          res + xShow.show(e) + ", "
+        } else {
+          res + xShow.show(e) + "]"
+        }
+      }
+    }
   }
 }
