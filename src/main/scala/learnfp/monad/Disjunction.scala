0b10a5e71f23ab725bb4785e71a2ec3bbe2f9791
Gino Emiliozzi <ginoemiliozzi11@gmail.com>
2020-03-14 16:21:12 -0300

    Monad exercises

--- src/main/scala/learnfp/monad/Disjunction.scala
+++ src/main/scala/learnfp/monad/Disjunction.scala
@@ -4,9 +4,15 @@ import learnfp.functor.Disjunction._
 import learnfp.functor.DisjunctionInstance._
 
 object DisjunctionInstance {
-  implicit def disjunctionMonadInstance[L] = new Monad[({type E[A] = Disjunction[L, A]})#E]() {
-    override def pure[A](a: A): Disjunction[L, A] = ???
-    override def flatMap[A, B](a: Disjunction[L, A])(fx: A => Disjunction[L, B]): Disjunction[L, B] = ???
+  implicit def disjunctionMonadInstance[L] =
+    new Monad[({type E[A] = Disjunction[L, A]})#E]() {
+    override def pure[A](a: A): Disjunction[L, A] = RightDisjunction(a)
+    override def flatMap[A, B]
+    (a: Disjunction[L, A])(fx: A => Disjunction[L, B])
+    : Disjunction[L, B] = a match {
+      case RightDisjunction(rightValue) => fx(rightValue)
+      case LeftDisjunction(leftValue) => LeftDisjunction[L, B](leftValue)
+    }
   }
 
   implicit def disjunctionToMonadOps[L, R](disjunction:Disjunction[L, R]) = new MonadOps[R, ({type E[X] = Disjunction[L, X]})#E](disjunction)
