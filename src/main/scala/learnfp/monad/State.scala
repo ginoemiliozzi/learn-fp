bebaef1b0d18ff1c1a702c1df91f50a6f30d9937
ginoemiliozzi <ginoemiliozzi11@gmail.com>
2020-10-23 10:24:31 +0100

    Monad exercises - state and writer

--- src/main/scala/learnfp/monad/State.scala
+++ src/main/scala/learnfp/monad/State.scala
@@ -5,8 +5,11 @@ import learnfp.functor.StateInstance._
 
 object StateInstance {
   implicit def stateMonadInstance[S] = new Monad[({type E[X] = State[S, X]})#E]() {
-    override def pure[A](a: A): State[S, A] = ???
-    override def flatMap[A, B](a: State[S, A])(fx: A => State[S, B]): State[S, B] = ???
+    override def pure[A](a: A): State[S, A] = new State(s => (s, a))
+    override def flatMap[A, B](a: State[S, A])(fx: A => State[S, B]): State[S, B] = State { s =>
+      val (s1, a1) = a.run(s)
+      fx(a1).run(s1)
+    }
   }
 
   implicit def stateToMonadOps[S, A](a:State[S, A]) = new MonadOps[A, ({type E[X] = State[S, X]})#E](a)
